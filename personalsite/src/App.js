570efa62b9e75b0fc9dbe54dff89f6b108c286be
Justin <justinroesner@gmail.com>
2021-12-21 12:17:30 -0600

    initial push

---
+++ personalsite/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <h1> Justin Roesner </h1>
+          Personal Website <br/>
+          <a href="http://eqtimer.hugesalt.com" target="_blank">Timer</a>
+        </p>
+        <br/>
+        <p>
+          Mahasin <br/>
+
+          <img href="./assets/salt.jpg"></img>
+        </p>
+
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
