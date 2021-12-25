a130432bb4e315bc60abe0f0ebface0da24e69bd
Justin <justinroesner@gmail.com>
2021-12-24 20:04:15 -0600

    new theme, fake links

--- personalsite/src/App.js
+++ personalsite/src/App.js
@@ -1,4 +1,6 @@
 import logo from './logo.svg';
+import salt from './assets/salt.jpg';
+
 import './App.css';
 
 function App() {
@@ -6,18 +8,21 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          <h1> Justin Roesner </h1>
-          Personal Website <br/>
+          <h1 id="NameTitle"> Hi, I'm Justin Roesner. </h1>
+          <div>I'm a front-end web developer.</div> <br/>
+          Personal Projects:🧂<br/>
           <a href="http://eqtimer.hugesalt.com" target="_blank">Timer</a>
+          <a id="l" href="http://eqtimer.hugesalt.com" target="_blank">Github</a><br/>
+          <a href="http://eqtimer.hugesalt.com" target="_blank">Mind Map</a>
+          <a id="l" href="http://eqtimer.hugesalt.com" target="_blank">Github</a><br/>
+          <a href="http://eqtimer.hugesalt.com" target="_blank">Racing Game</a>
+          <a id="l" href="http://eqtimer.hugesalt.com" target="_blank">Github</a><br/>
         </p>
         <br/>
         <p>
           Mahasin <br/>
-
-          <img href="./assets/salt.jpg"></img>
+          <img src={salt} alt="random image of a salt mine" />
         </p>
-
-        
       </header>
     </div>
   );
